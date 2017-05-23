1789189cf5c673b937ecc4b791bc9500c6a3b4c5
Marcy <mlsantoyo@att.net>
2017-05-23 11:03:12 -0400

    update1

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+
+
+//$(document).ready(function(){}
+
+
+var green = "";
+var orange = "";
+var blue = "";
+var red = "";
+
+//Generate random number 
+var randomNumber = Math.floor(Math.random() * ((120-19)+1) + 19);
+console.log(randomNumber);
+
+//display in div called random
+
+
+//generate random number for a crystal
+ var crystalScore = Math.floor(Math.random() * ((12-1)+1) + 1);
+ console.log(crystalScore);
+
+// //game starts
+
+
+
