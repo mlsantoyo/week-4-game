bda14856881e29f7c88551b6f7eb118a46f19bff
Marcy <mlsantoyo@att.net>
2017-05-23 21:29:31 -0400

    fixed crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,64 @@
 
 
-//$(document).ready(function(){}
-
-
-var green = "";
-var orange = "";
-var blue = "";
-var red = "";
 
 //Generate random number 
 var randomNumber = Math.floor(Math.random() * ((120-19)+1) + 19);
 console.log(randomNumber);
 
+var currentRound = 0;
+
 //display in div called random
+$("#target").html(randomNumber);
 
 
-//generate random number for a crystal
- var crystalScore = Math.floor(Math.random() * ((12-1)+1) + 1);
- console.log(crystalScore);
+//generate random number for each crystal
+var green = Math.floor(Math.random() * ((12-1)+1) + 1);
+$()
 
-// //game starts
 
+var orange = Math.floor(Math.random() * ((12-1)+1) + 1);
+
+
+var blue = Math.floor(Math.random() * ((12-1)+1) + 1);
+
+
+var red = Math.floor(Math.random() * ((12-1)+1) + 1);
+
+
+$("#green").on("click", function(){
+	console.log("clicked green: ", green);
+// currentRound = currentRound + green;
+currentRound += green;
+
+console.log("currentRound", currentRound);	
+$("#currentRound").html(currentRound);
+});
+
+$("#orange").on("click", function(){
+	console.log("clicked orange: ", orange);
+
+	currentRound += orange;
+
+	console.log("currentRound", currentRound);
+	$("#currentRound").html(currentRound);		
+});
+
+$("#blue").on("click", function(){
+	console.log("clicked blue: ", blue);
+
+	currentRound += blue;
+
+	console.log("currentRound", currentRound);
+	$("#currentRound").html(currentRound);		
+});
+
+$("#red").on("click", function(){
+	console.log("clicked red: ", red);
+
+	currentRound += red;
+
+	console.log("currentRound", currentRound);
+	$("#currentRound").html(currentRound);		
+});
 
 
