1f2cbd5768dd17d91cfe25669f4c75576ee68a88
Marcy <mlsantoyo@att.net>
2017-05-25 12:54:31 -0400

    works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,14 @@ var randomNumber = Math.floor(Math.random() * ((120-19)+1) + 19);
 console.log(randomNumber);
 
 var currentRound = 0;
+var wins = 0;
+var losses = 0;
+
+
+//display wins and losses
+$('#numberWins').html(wins);
+$('#numberLosses').html(losses);
+
 
 //display in div called random
 $("#target").html(randomNumber);
@@ -13,25 +21,62 @@ $("#target").html(randomNumber);
 
 //generate random number for each crystal
 var green = Math.floor(Math.random() * ((12-1)+1) + 1);
-$()
-
-
 var orange = Math.floor(Math.random() * ((12-1)+1) + 1);
-
-
 var blue = Math.floor(Math.random() * ((12-1)+1) + 1);
-
-
 var red = Math.floor(Math.random() * ((12-1)+1) + 1);
 
 
+
+
+//start new round
+function newRound(){
+	randomNumber = Math.floor(Math.random() * ((120-19)+1) + 19);
+	console.log(randomNumber);
+	$("#target").html(randomNumber);
+	green= Math.floor(Math.random()*((12-1)+1) + 1);
+	orange= Math.floor(Math.random()*((12-1)+1) + 1);
+	blue= Math.floor(Math.random()*((12-1)+1) + 1);
+	red= Math.floor(Math.random()*((12-1)+1) + 1);
+	currentRound = 0;
+	$("#currentRound").html(currentRound);
+  } 
+
+//adds the wins to the userTotal
+function youWin(){
+alert("You're a winner!");
+  wins++; 
+  $('#numberWins').html(wins);
+ 
+}
+//addes the losses to the userTotal
+function youLose(){
+alert ("Sorry :( You lost!");
+  losses++;
+  $('#numberLosses').html(losses);
+  
+}
+
+
+
 $("#green").on("click", function(){
+	
 	console.log("clicked green: ", green);
 // currentRound = currentRound + green;
 currentRound += green;
 
+
 console.log("currentRound", currentRound);	
 $("#currentRound").html(currentRound);
+
+    //sets win/lose conditions
+        if (currentRound == randomNumber){
+          youWin();
+          newRound();
+        }
+        else if ( currentRound > randomNumber){
+          youLose();
+          newRound();
+        }   
 });
 
 $("#orange").on("click", function(){
@@ -40,7 +85,17 @@ $("#orange").on("click", function(){
 	currentRound += orange;
 
 	console.log("currentRound", currentRound);
-	$("#currentRound").html(currentRound);		
+	$("#currentRound").html(currentRound);	
+
+	//sets win/lose conditions
+        if (currentRound == randomNumber){
+          youWin();
+          newRound();
+        }
+        else if ( currentRound > randomNumber){
+          youLose();
+          newRound();
+        }   	
 });
 
 $("#blue").on("click", function(){
@@ -49,7 +104,17 @@ $("#blue").on("click", function(){
 	currentRound += blue;
 
 	console.log("currentRound", currentRound);
-	$("#currentRound").html(currentRound);		
+	$("#currentRound").html(currentRound);	
+
+	//sets win/lose conditions
+        if (currentRound == randomNumber){
+          youWin();
+          newRound();
+        }
+        else if ( currentRound > randomNumber){
+          youLose();
+          newRound();
+        }   	
 });
 
 $("#red").on("click", function(){
@@ -58,7 +123,17 @@ $("#red").on("click", function(){
 	currentRound += red;
 
 	console.log("currentRound", currentRound);
-	$("#currentRound").html(currentRound);		
+	$("#currentRound").html(currentRound);
+
+	//sets win/lose conditions
+        if (currentRound == randomNumber){
+          youWin();
+          newRound();
+        }
+        else if ( currentRound > randomNumber){
+          youLose();
+          newRound();
+        }   		
 });
 
 
